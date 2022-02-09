797e39a3af1aa86c4d5cd55ad72a4236d63a5157
manas522 <manas522@gmail.com>
2022-02-09 21:53:13 +0530

    https://leetcode.com/problems/number-of-1-bits

---
+++ number-of-1-bits-easy.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n - a positive integer
+ https://leetcode.com/problems/number-of-1-bits
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let left = 32;
+    let right = 0;
+    let power = 0;
+    while(left > right) {
+        const mid = ((left + right) / 2) | 0;
+        if (2 ** mid === n)  {
+            leftMost = mid;
+            break;
+        }
+        if (2 ** mid > n) {
+            left = mid - 1
+        } else {
+            right = mid + 1;
+        }
+        power = left;
+    }
+    let count = 0;
+    let nq = n;
+    while(power >= 0) {
+        const q = (2 ** power);
+         remainder = (nq/q) | 0;
+         nq = nq % q;
+        if (remainder) {
+            count++;
+        }
+        power--;
+        
+    }
+    return count;
+};
